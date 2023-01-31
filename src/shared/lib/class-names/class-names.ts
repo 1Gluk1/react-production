27e72d41c3177c17be683bbac93b56aa3688bfb2
Anton Gorbachev <shieldmedia@yandex.ru>
2023-01-31 23:42:55 +0700

    fix(class-names): add filter for undefined value

--- src/shared/lib/class-names/class-names.ts
+++ src/shared/lib/class-names/class-names.ts
@@ -3,7 +3,7 @@ type TMods = Record<string, boolean | string>
 export function classNames(className: string, mods: TMods = {}, additional: string[] = []): string {
     return [
         className,
-        ...additional,
+        ...additional.filter(Boolean),
         Object.entries(mods)
             .filter(([className, value]) => Boolean(value))
             .map(([className]) => className)
