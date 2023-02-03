1486ce711b1ef7dc747be21cf9747ebf93c96f13
Anton Gorbachev <shieldmedia@yandex.ru>
2023-02-04 03:15:31 +0700

    fix(loader): add translation for loader content

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,9 +1,12 @@
 import {Suspense} from 'react';
 import {Route, Routes} from 'react-router-dom';
 import {routesConfig} from 'shared/config/routes/routes.config';
+import {useTranslation} from 'react-i18next';
 
-const AppRouter = () => (
-	<Suspense fallback={<div>Загрузка...</div>}>
+const AppRouter = () => {
+	const {t} = useTranslation();
+
+	return <Suspense fallback={<div>{t('loading')}</div>}>
 		<Routes>
 			{routesConfig.map(({path, element}) => (
 				<Route
@@ -17,7 +20,7 @@ const AppRouter = () => (
 				/>
 			))}
 		</Routes>
-	</Suspense>
-);
+	</Suspense>;
+};
 
 export default AppRouter;
