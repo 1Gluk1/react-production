e24fa24c97cfe5c550d5899ef3283f1512cfe7ea
Anton Gorbachev <shieldmedia@yandex.ru>
2023-02-04 18:59:16 +0700

    feat(ErrorBoundary): add BugButton in MainPage for testing purpose

--- src/app/providers/ErrorBoundary/ui/BugButton.tsx
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -1,7 +1,10 @@
 import {Button} from 'shared/ui/Button/Button';
 import {useEffect, useState} from 'react';
+import {useTranslation} from 'react-i18next';
 
+// Only for testing
 export const BugButton = () => {
+	const {t} = useTranslation();
 	const [error, setError] = useState<boolean>(false);
 
 	const throwError = () => {
@@ -9,8 +12,10 @@ export const BugButton = () => {
 	};
 
 	useEffect(() => {
-		throw new Error();
+		if (error) {
+			throw new Error();
+		}
 	}, [error]);
 
-	return (<Button onClick={throwError}>Throw error</Button>);
+	return (<Button onClick={throwError}>{t('throw_error')}</Button>);
 };
